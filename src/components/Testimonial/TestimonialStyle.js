2a16ef55732ff5d928ec7405985c65afe802731a
piusprince <45078705+piusprince@users.noreply.github.com>
2021-11-09 07:54:02 +0000

    Create TestimonialStyle.js
    styling for testimonial section

---
+++ src/components/Testimonial/TestimonialStyle.js
@@ -0,0 +1,60 @@
+import styled from 'styled-components';
+
+export const TestimonialWrapper = styled.section`
+    margin: 5rem;
+`
+
+export const TestimonialTitle = styled.h1`
+    text-align: center;
+    margin-bottom: 5rem;
+    font-size: 3.9rem;
+`
+
+export const TestimonialBg = styled.div`
+
+    background-image: linear-gradient(105.76deg, #FEE9CF 32.12%, rgba(236, 135, 228, 0) 102.82%);
+    border-radius: 2.8rem
+`
+
+export const Wrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 5rem;
+    padding: 5rem;
+`
+
+export const TestimonialItem = styled.figure`
+    padding: 5rem;
+    background-color: white;
+    border-radius: 2.8rem;
+    z-index: 500;
+
+
+    &:first-child {
+        border: 1px solid #EC87E4;
+    }
+
+    &:nth-child(3) {
+        border: 1px solid #FEE9CF;
+    }
+
+`
+
+export const TestimonialDesc = styled.p`
+    font-size: 1.6rem;
+`
+
+export const TestimonialImg = styled.img`
+    margin: 2rem 0;
+    border-radius: 50%;
+    height: 5.8rem;
+    width: 5.8rem;
+`
+
+export const TestimonialName = styled.h3`
+
+`
+
+export const TestimonialJob = styled.h4`
+
+`
